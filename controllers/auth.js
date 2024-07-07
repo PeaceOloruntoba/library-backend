3800b505fa07d5e6a4bcd0889ad9b8d625716c52
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-07-07 05:06:19 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-import {UserModel} from "../models/user.js";
+// import {UserModel} from "../models/user.js";
 
 export const createUser = async (req, res) => {
   const { email, name, password } = req.body;
   await UserModel.create({ email, password, name });
-  res.json(req.body);
+  console.log(req?.body);
+  res.json(req?.body);
 };
