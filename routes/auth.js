65cdc152cf38c01ca4f25dcf07597ee34be56d69
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 11:34:22 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,12 @@
 import { Router } from "express";
 import { createUser, privateResponse, signin } from "../controllers/auth.js";
 import { newUserValidator } from "../middleware/validator.js";
-import jwt from "jsonwebtoken";
 import { authorizATion } from "../middleware/authorization.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", newUserValidator, createUser);
 authRouter.post("/signin", newUserValidator, signin);
-authRouter.get(
-  "/private", authorizATion,
-  privateResponse
-);
+authRouter.get("/private", authorizATion, privateResponse);
 
 export default authRouter;
