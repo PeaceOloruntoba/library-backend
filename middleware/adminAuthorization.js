ca260be94c9481fc2140e543176c7580ab703b24
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 11:39:21 +0100

    Update adminAuthorization.js

--- middleware/adminAuthorization.js
+++ middleware/adminAuthorization.js
@@ -1,13 +1,4 @@
-import jwt from "jsonwebtoken";
-import { UserModel } from "../models/user.js";
-
 export const adminAuthorization = async (req, res, next) => {
-  const authorizationToken = req.headers.authorization;
-  const token = authorizationToken?.split("Bearer ")[1];
-  if (!token) return res.status(403).json({ error: "unauthorized access!" });
-  const payload = jwt.verify(token, "secret");
-  const user = await UserModel.findById(payload.id);
-  if (!user) return res.status(403).json({ error: "unauthorized access!" });
-  req.user = user;
+  
   next();
 };
