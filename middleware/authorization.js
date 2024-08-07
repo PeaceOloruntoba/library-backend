68450a7ea3b4813c582f8debb4c9ad39500ba39b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 11:29:49 +0100

    suuu

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,4 +1,10 @@
-const authorization = () => {
-
-}
-
+const authorization = async (req, res, next) => {
+  const authorizationToken = req.headers.authorization;
+  const token = authorizationToken?.split("Bearer ")[1];
+  if (!token) return res.status(403).json({ error: "unauthorized access!" });
+  const payload = jwt.verify(token, "secret");
+  const user = await UserModel.findById(payload.id);
+  if (!user) return res.status(403).json({ error: "unauthorized access!" });
+  req.user = user;
+  next();
+};
--- routes/auth.js
+++ routes/auth.js
@@ -9,17 +9,7 @@ const authRouter = Router();
 authRouter.post("/signup", newUserValidator, createUser);
 authRouter.post("/signin", newUserValidator, signin);
 authRouter.get(
-  "/private",
-  async (req, res, next) => {
-    const authorizationToken = req.headers.authorization;
-    const token = authorizationToken?.split("Bearer ")[1];
-    if (!token) return res.status(403).json({ error: "unauthorized access!" });
-    const payload = jwt.verify(token, "secret");
-    const user = await UserModel.findById(payload.id);
-    if (!user) return res.status(403).json({ error: "unauthorized access!" });
-    req.user = user;
-    next();
-  },
+  "/private", authorization,
   privateResponse
 );
 
