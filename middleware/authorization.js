a755f0ac37997b272dabc20fce1c9c403bbcab8e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 12:16:54 +0100

    Update authorization.js

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -4,22 +4,20 @@ const { JsonWebTokenError } = pkg;
 import { UserModel } from "../models/user.js";
 
 export const authorizATion = async (req, res, next) => {
-    try {
-          const authorizationToken = req.headers.authorization;
-  const token = authorizationToken?.split("Bearer ")[1];
-  if (!token) return res.status(403).json({ error: "unauthorized access!" });
-  const payload = jwt.verify(token, "secret");
-  const user = await UserModel.findById(payload.id);
-  if (!user) return res.status(403).json({ error: "unauthorized access!" });
-  req.user = user;
-  next();
-    } catch (error) {
-        if(error instanceof JsonWebTokenError){
-            res.status(403).json({ error: "unauthorized access!" });
-        }
-        else {
-            res.status(500).json({ error: "Something went wrong!" });
-        }
+  try {
+    const authorizationToken = req.headers.authorization;
+    const token = authorizationToken?.split("Bearer ")[1];
+    if (!token) return res.status(403).json({ error: "unauthorized access!" });
+    const payload = jwt.verify(token, "secret");
+    const user = await UserModel.findById(payload.id);
+    if (!user) return res.status(403).json({ error: "unauthorized access!" });
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error instanceof JsonWebTokenError) {
+      res.status(403).json({ error: "unauthorized access!" });
+    } else {
+      res.status(500).json({ error: "Something went wrong!" });
     }
-
+  }
 };
