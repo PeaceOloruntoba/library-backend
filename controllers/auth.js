18ffb56463402dc57f27c58fba252118a245efc7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 10:49:10 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,11 @@ import { UserModel } from "../models/user.js";
 
 export const createUser = async (req, res) => {
   const { email, name, password } = req.body;
+  const oldUser = await UserModel.findOne({ email });
+  if (oldUser)
+    return res
+      .status(403)
+      .json({ error: "Account with this email already exist" });
   const user = await UserModel.create({ email, password, name });
   res.json({ success: true, user });
 };
