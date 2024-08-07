236b45647899cd9c4a7ae7e677e5caede55aa5b6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 12:18:42 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
-import { adminResponse, createUser, privateResponse, signin } from "../controllers/auth.js";
+import {
+  adminResponse,
+  createUser,
+  privateResponse,
+  signin,
+} from "../controllers/auth.js";
 import { newUserValidator } from "../middleware/validator.js";
-import { authorizATion } from "../middleware/authorization.js";
-import { adminAuthorization } from "../middleware/adminAuthorization.js";
+import {
+  adminAuthorization,
+  userAuthorization,
+} from "../middleware/authorization.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", newUserValidator, createUser);
 authRouter.post("/signin", newUserValidator, signin);
-authRouter.get("/private", authorizATion, privateResponse);
-authRouter.get("/admin", authorizATion, adminAuthorization, adminResponse);
+authRouter.get("/private", userAuthorization, privateResponse);
+authRouter.get("/admin", userAuthorization, adminAuthorization, adminResponse);
 
 export default authRouter;
