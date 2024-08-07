6cf78b31266a41b42565270a0a1778895ab364a2
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 12:18:03 +0100

    Update adminAuthorization.js

--- middleware/adminAuthorization.js
+++ middleware/adminAuthorization.js
@@ -1,4 +1,4 @@
-export const adminAuthorization = async (req, res, next) => {
-  if (req.user.role === "admin") next();
-  else res.status(403).json({ error: "Protected only for admin" });
-};
+// export const adminAuthorization = async (req, res, next) => {
+//   if (req.user.role === "admin") next();
+//   else res.status(403).json({ error: "Protected only for admin" });
+// };
