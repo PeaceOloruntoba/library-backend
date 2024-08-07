0bb96a22b3fbb449a0375c5d2cfa1de6d32c0126
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 11:35:15 +0100

    Update authorization.js

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { UserModel } from "../models/user";
+import { UserModel } from "../models/user.js";
 
 export const authorizATion = async (req, res, next) => {
   const authorizationToken = req.headers.authorization;
