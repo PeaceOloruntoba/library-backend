3787b916b97cc765cafe32afb741ab3518b73cf1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 11:24:09 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUser, privateResponse, signin } from "../controllers/auth.js";
 import { newUserValidator } from "../middleware/validator.js";
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
@@ -10,8 +11,10 @@ authRouter.get(
   "/private",
   (req, res, next) => {
     const authorizationToken = req.headers.authorization;
-    const token = authorizationToken?.split("Bearer ");
-    console.log(token);
+    const token = authorizationToken?.split("Bearer ")[1];
+    if (!token) return res.status(403).json({ error: "unauthorized access!" });
+    const payload = jwt.verify(token, "secret");
+    console.log(payload.id);
   },
   privateResponse
 );
