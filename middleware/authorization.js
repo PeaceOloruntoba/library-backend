be5b1a90ad9b4120271fa772e44ad1923cca4849
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-07 12:17:54 +0100

    Update authorization.js

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -21,3 +21,9 @@ export const userAuthorization = async (req, res, next) => {
     }
   }
 };
+
+export const adminAuthorization = async (req, res, next) => {
+  if (req.user.role === "admin") next();
+  else res.status(403).json({ error: "Protected only for admin" });
+};
+
