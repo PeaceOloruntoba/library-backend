a6d68b696149717b7b11e2a9ef4ebeb5fb1f19c6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-15 09:46:57 +0100

    multer

--- routes/book.js
+++ routes/book.js
@@ -1,16 +1,27 @@
 import { Router } from "express";
-import { createBook, deleteBook, fetchBook, fetchBooks, updateBook } from "../controllers/book";
+import {
+  createBook,
+  deleteBook,
+  fetchBook,
+  fetchBooks,
+  updateBook,
+} from "../controllers/book";
 import { adminAuthorization } from "../middleware/authorization";
 import multer from "multer";
+
 const upload = multer({ dest: "uploads/" });
 
 const bookRouter = Router();
-
 bookRouter.post("/book", adminAuthorization, upload.single("file"), createBook);
-bookRouter.patch("/book", adminAuthorization, updateBook);
+bookRouter.patch(
+  "/book/:id",
+  adminAuthorization,
+  upload.single("file"),
+  updateBook
+);
+bookRouter.delete("/book/:id", adminAuthorization, deleteBook);
+
 bookRouter.get("/book", fetchBooks);
-bookRouter.get("/book", fetchBook);
-bookRouter.delete("/book", deleteBook);
+bookRouter.get("/book/:id", fetchBook);
 
 export default bookRouter;
-
