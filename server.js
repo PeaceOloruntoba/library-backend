7077ae7e95225f62a613a6e1c59eff66561e0953
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-15 17:50:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ import "./db/index.js";
 import authRouter from "./routes/auth.js";
 import bookRouter from "./routes/book.js";
 import adminRoute from "./routes/adminRoute.js";
-import {
-  adminAuthorization,
-  userAuthorization,
-} from "./middleware/authorization.js";
+import { userAuthorization } from "./middleware/authorization.js";
 
 const app = express();
 
@@ -14,7 +11,7 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/", bookRouter);
-app.use("/admin", userAuthorization, adminAuthorization, adminRoute);
+app.use("/admin", userAuthorization, adminRoute);
 
 app.get("/", (req, res) => {
   res.json({
