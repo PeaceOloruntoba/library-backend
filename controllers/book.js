189b2db4d55e7cb48345521b4320755b0a5e7825
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-08-15 09:35:04 +0100

    Update book.js

--- controllers/book.js
+++ controllers/book.js
@@ -1,13 +1,29 @@
 import { Book, BookModelModel } from "../models/book.js";
 
 export const createBook = async (req, res) => {
-  const { name, description, file } = req.body;
-  const oldBook = await BookModel.findOne({ name });
-  if (oldBook)
-    return res.status(403).json({ error: "Book with this name already exist" });
-  const book = await BookModel.create({ name, description, file });
-  res.json({ success: true, book });
+  const { name, description } = req.body;
+  const file = req.file;
+  if (!file) {
+    return res.status(400).json({ error: "Please upload a file." });
+  }
+  try {
+    const oldBook = await BookModel.findOne({ name });
+    if (oldBook)
+      return res
+        .status(403)
+        .json({ error: "Book with this name already exists" });
+
+    const book = await BookModel.create({
+      name,
+      description,
+      file: file.path,
+    });
+    res.json({ success: true, book });
+  } catch (error) {
+    res.status(500).json({ error: "Error creating the book" });
+  }
 };
+
 
 export const fetchBooks = async (req, res) => {
   try {
@@ -23,7 +39,7 @@ export const fetchBook = async (req, res) => {
   try {
     const book = await BookModel.findById(id);
     if (!book) {
-      return res.status(404).json({ error: "Book not found" });
+      return res.status(404).json({ error: "No book with provided id" });
     }
     res.json({ success: true, book });
   } catch (error) {
@@ -31,13 +47,14 @@ export const fetchBook = async (req, res) => {
   }
 };
 
+
 export const updateBook = async (req, res) => {
   const { id } = req.params;
   const { name, description, file } = req.body;
   try {
     const book = await BookModel.findById(id);
     if (!book) {
-      return res.status(404).json({ error: "Book not found" });
+      return res.status(404).json({ error: "No book with provided id" });
     }
     book.name = name || book.name;
     book.description = description || book.description;
@@ -49,4 +66,16 @@ export const updateBook = async (req, res) => {
   }
 };
 
-export const deleteBook = async (req, res) => {};
+export const deleteBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await BookModel.findById(id);
+    if (!book) {
+      return res.status(404).json({ error: "No book with provided id" });
+    }
+    await BookModel.deleteOne({ _id: id });
+    res.json({ success: true, message: "Book deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting the book" });
+  }
+};
